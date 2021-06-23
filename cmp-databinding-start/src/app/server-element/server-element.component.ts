94c8f6968f2a5f021028114faafbc1811fcd9ba9
Mena Sergeyous <Menas@localgovcorp.com>
2021-06-23 15:25:06 -0500

    ngAfterContentInit

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,11 +6,48 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // is the default but you can switch between None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit,OnChanges, 
+DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
-
+  @Input() name : string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  constructor() { 
+      console.log("constructor called");
+  }
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
+  
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
   ngOnInit(): void {
+    console.log('ngOnInit called');
+    console.log("Text Content " + this.header.nativeElement.textContent);
+    console.log('Text Content of Paragraph : ' + this.paragraph.nativeElement.textContent);
+  }
+  ngDoCheck(){// it will slow down the appication if the application is hug 
+    console.log('ngDoCheck called!');
   }
 
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit called!");
+    console.log('Text Content of Paragraph : ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked called!");
+  }
+ 
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit called!");
+    console.log("Text Content " + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked called!");
+  }
+
+  ngOnDestroy(){
+    console.log("ngOnDestory called!")
+  }
 }
